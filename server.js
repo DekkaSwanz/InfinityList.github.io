c895a0474cbe10fd16bf2b6a2d1f838ac0e936d8
Derrick Davis <dhdavis@utah.gov>
2019-04-19 16:54:11 -0600

    cors again

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const cors = require('cors');
 const app = express();
 
 //cross domain workaround
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors())
 // Bodyparser middleware
 app.use(bodyParser.json());
 
