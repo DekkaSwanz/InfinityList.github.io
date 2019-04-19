03a82b2293df1f16014fd10dbbdd740c7b3bc0b4
Derrick Davis <dhdavis@utah.gov>
2019-04-19 14:05:07 -0600

    cross origin fix again

--- server.js
+++ server.js
@@ -10,8 +10,12 @@ const app = express();
 // cross domain workaround
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT','POST','PATCH','DELETE','GET');
+        return res.status(200).json({});
+    }
+    next(error);
   });
 
 // Bodyparser middleware
