cfb36695c7ed423a9ff22a5416c41a862f70c8e1
Derrick Davis <dhdavis@utah.gov>
2019-04-19 14:37:28 -0600

    error fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Methods', 'PUT','POST','PATCH','DELETE','GET');
         return res.status(200).json({});
     }
-    next(error);
+    next();
   });
 
 // Bodyparser middleware
