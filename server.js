a99faa31ae98e61821a765c145da179ddc222eb3
Derrick Davis <dhdavis@utah.gov>
2019-04-19 16:11:03 -0600

    test 2

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path')
 const items = require('./routes/api/items')
-const cors = require('cors');
 
 const app = express();
 
 //cross domain workaround
-
+app.use((req,res,next) =>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next();
+})
 // Bodyparser middleware
 app.use(bodyParser.json());
 
