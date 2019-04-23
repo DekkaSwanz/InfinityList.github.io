ca2933b5b6bfba3d92ca1b4980c04eae1651edce
Derrick Davis <dhdavis@utah.gov>
2019-04-23 08:20:21 -0600

    port fix

--- server.js
+++ server.js
@@ -20,14 +20,17 @@ mongoose
     .then(() => console.log('mongoDB Connected...'))
     .catch(err => console.log(err));
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 // serve static assets in production
 if(process.env.NODE_ENV === 'production'){
+    console.log('in production')
     //set static folder
     app.use(express.static('client/build'));
     app.get('*',(req,res) => {
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
+}else{
+    console.log('in development')
 }
 // Use Routes 
 app.use('/api/items',items)
